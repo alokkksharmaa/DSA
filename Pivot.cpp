9da751c4416385bfe91f43385918f75e82bcbf79
Alok Kumar Sharma <143309447+alokkksharmaa@users.noreply.github.com>
2025-10-09 18:10:36 +0530

    Add files via upload
    Signed-off-by: Alok Kumar Sharma <143309447+alokkksharmaa@users.noreply.github.com>

---
+++ Pivot.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int findPivot(int arr[], int n)
+{
+    int s = 0;
+    int end = n - 1;
+
+    while (s <= end)
+    {
+        int mid = s + (end - s) / 2;
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    int arr[6] = {8, 10, 12, 17, 6, 23};
+
+    cout << "Pivot is : " << findPivot(arr, 6) << endl;
+}
