213c0fc15686191522092aca2fcdc2694a0bb8f6
alokksharmaa <aloksharma1097@gmail.com>
2025-12-16 19:22:59 +0530

    dummy

--- Arrays/maximumAverageSubArray.cpp
+++ Arrays/maximumAverageSubArray.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double findMaxAverage(int nums[], int n, int k){
-
-  int sum = 0; 
-  for(int i=0; i<k;i++){
+double findMaxAverage(int nums[], int n, int k)
+{
+  int sum = 0;
+  for (int i = 0; i < k; i++)
+  {
     sum += nums[i];
   }
 
   int maxSum = sum;
-  int start =  0;
-  for(int end=k; end<n; end++){
+  int start = 0;
+
+  for (int end = k; end < n; end++)
+  {
     sum -= nums[start++];
     sum += nums[end];
 
     maxSum = max(maxSum, sum);
   }
-
-  return (double) maxSum / k;
-
+  
+  return (double)maxSum / k;
 }
-
 
 int main()
 {
-
   int nums[] = {1,12,-5,-6,50,3};
-  int n = sizeof(nums)/sizeof(nums[0]);
+  int n = sizeof(nums) / sizeof(nums[0]);
 
   int k = 4;
 
-  cout << "MaxAverage of SubArray is: " << findMaxAverage(nums, n, k) << endl;
-    
-  return 0;
+  cout <<"Average maximum Sum is: " << findMaxAverage(nums, n , k) << endl;
 }
