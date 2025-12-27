c6ec21e9ac2d3655d51342b8c3d4668f2d1bf5be
Alok Kumar Sharma <aloksharma1097@gmail.com>
2025-12-27 16:07:25 +0530

    linked list

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -1,43 +1,37 @@
 #include <iostream>
 using namespace std;
 
-// Node structure for the linked list
 class Node
 {
 public:
-  int data;   // stores the value
-  Node *next; // stores the address of next
-
-  Node(int val)
-  {
-    data = val;  // put the value inside the node
-    next = NULL; // new node doesn't point anywhere yet
-  }
+  int val;
+  Node *next;
 };
 
-// function to print the entire linked list
-void PrintList(Node *head)
+void PrintList(Node *n)
 {
-  Node *temp = head; // temp pointer is used to traverse 
-  // the entire linkedlist without loosing the head
-
-  while (temp != NULL)
+  while (n != 0)
   {
-    cout << temp->data << " ";  //print the value of the current node 
-    temp = temp->next;       //moves to the next node
+    cout << n->val << " ";
+    n = n->next;
   }
-  cout << endl;
-};
+}
 
 int main()
 {
-  Node *head = NULL;  //empty list initially
 
-  head = new Node(3);   // first node
-  head->next = new Node(5); //2nd Node
-  head->next->next = new Node(7);  //3rd Node
+  Node *head = new Node();
+  Node *second = new Node();
+  Node *third = new Node();
 
-  PrintList(head);   //prints 3 5 7
+  head->val = 1;
+  head->next = second;
+  second->val = 3;
+  second->next = third;
+  third->val = 4;
+  third->next = NULL;
+
+  PrintList(head);
 
   return 0;
 }
