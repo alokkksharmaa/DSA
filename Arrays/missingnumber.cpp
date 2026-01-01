cb1c1b87ce08f6e6e62c3ff16119edd2cceb2cff
Alok Kumar Sharma <aloksharma1097@gmail.com>
2026-01-01 10:24:53 +0530

    missing number in an array

---
+++ Arrays/missingnumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int MissingNumber(int nums[], int n)
+{
+
+  int sum1 = (n + 1) * (n + 2) / 2; // 1  to n+1
+
+  int sum2 = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum2 += nums[i];
+  };
+  return sum1 - sum2;
+};
+
+int main()
+{
+  int nums[] = {1, 3, 4};
+  int n = sizeof(nums) / sizeof(nums[0]);
+
+  cout << MissingNumber(nums, n) << endl;
+
+  return 0;
+}
--- LinkedList/singlylinkedlist.cpp
+++ LinkedList/singlylinkedlist.cpp
@@ -11,65 +11,37 @@ public:
   {
     this->data = data;
     this->next = nullptr;
-  };
+  }
 };
 
-class LinkedList
+// insert at head
+void insertAtHead(Node *&head, int data)
 {
-  Node *head;
+  Node *newNode = new Node(data); // creates a new Node
+  newNode->next = head;           // asssign new node to  the head
+  head = newNode;                 // now we link the new node with the head
+};
 
-public:
-  // constructor
-  LinkedList()
+void printList(Node *&head)
+{
+  Node *temp = head;
+
+  while (temp != nullptr)
   {
-    head = nullptr;
-  };
-
-  void insertAtHead(int data)
-  {
-    Node *newNode = new Node(data);
-
-    if (head == nullptr)
-    {
-      head = newNode;
-      return;
-    };
-
-    newNode->next = this->head;
-    this->head = newNode;
+    cout << temp->data << " ";
+    temp = temp->next;
   }
-
-  void Print()
-  {
-    Node *temp = head;
-
-    if (head == nullptr)
-    {
-      cout << "List empty" << endl;
-      return;
-    };
-
-    while (temp != NULL)
-    {
-      cout << temp->data << " ";
-      temp = temp->next;
-    };
-  };
+  cout << endl;
 };
 
 int main()
 {
-  LinkedList list;
+  Node *head = nullptr;
 
-  list.insertAtHead(29);
-  list.insertAtHead(39);
-  list.insertAtHead(23);
-  list.insertAtHead(79);
+  insertAtHead(head, 30);
+  insertAtHead(head, 50);
 
-  cout << "Elements of linkedList are:  ";
-
-  list.Print();
-  cout << endl;
+  printList(head);
 
   return 0;
-};
+}
