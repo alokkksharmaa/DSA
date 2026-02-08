ad6c1c1977c327f12982f8710ec50c16495bebbf
alokkksharmaa <aloksharma1097@gmail.com>
2026-02-08 16:46:02 +0530

    valid parenthesis

--- Stack/validParenthesis.cpp
+++ Stack/validParenthesis.cpp
@@ -2,9 +2,40 @@
 #include <stack>
 using namespace std;
 
+bool isValid(string s)
+{
+  stack<char> st;
+
+  for(int i = 0; i<s.size() ; i++)
+  {
+    if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+    {
+      st.push(s[i]);
+    }
+    else
+    {
+      if(st.empty())
+        return false;
+      char c = st.top();
+      st.pop();
+      if((s[i] == ')' && c != '(') ||
+        (s[i] == ']' && c != '[') || 
+        (s[i] == '}' && c != '{'))
+        return false;
+    }
+  }
+  return st.empty();
+}
+
+
 int main()
 {
-  
+  string s = "({[]})";
+  if(isValid(s))
+    cout << "Valid Parenthesis" << endl;
+  else
+    cout << "Invalid Parenthesis" << endl;
     
+
   return 0;
 }
