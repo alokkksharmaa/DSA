e7982a76c8930c281f3bfdd1dd7cb6abd22def2f
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-18 00:11:32 +0530

    char & string arrays

--- Char & String Arrays/longestsubstr.cpp
+++ Char & String Arrays/longestsubstr.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
+
+// longest substr without repeating characher
+
+int substr(string str, int n)
+{
+
+  int maxlen = 0;
+  for (int i = 0; i < n; i++)
+  {
+    unordered_set<char> st;
+
+    for (int j = i; j < n; j++)
+    {
+
+      if (st.count(str[j]))
+        break;
+
+      st.insert(str[j]);
+
+      maxlen = max(maxlen, j - i + 1);
+    }
+  }
+  return maxlen;
+}
 
 int main()
 {
   string str = "cadbzabcd";
-  int n = sizeof(str)/sizeof(str[0]);
-
+  int n = str.length();
   
+  cout << substr(str, n);
 
   return 0;
 }
