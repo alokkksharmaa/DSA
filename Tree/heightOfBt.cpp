0b756a15e66d19f8262c578fb56927dacdd52bb6
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-17 13:40:13 +0530

    tree files added: height of Binary Tree

--- Tree/heightOfBt.cpp
+++ Tree/heightOfBt.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
+int heightofTree(Node *root)
+{
+
+  if (root == nullptr)
+  {
+    return 0;
+  };
+
+  int leftHeight = heightofTree(root->left);
+  int rightHeight = heightofTree(root->right);
+
+  return 1 + max(leftHeight, rightHeight);
+}
 
 int main()
 {
-    
+  // Let's build a quick 3-level tree
+  //       1
+  //      / \
+  //     2   3
+  //    /     
+  //   4     
+  Node *root = new Node(1);
+  root->left = new Node(2);
+  root->left = new Node(3);
+  root->left->left = new Node(4);
+  cout << "Tree Height: " << heightofTree(root) << endl;
+
   return 0;
 }
