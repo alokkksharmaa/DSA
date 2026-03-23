c71dea7eb2023ace1ddc64ec1df1d7be8bfc7439
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-23 22:08:18 +0530

    combination sum using recursion

--- Char & String Arrays/combinationSum.cpp
+++ Char & String Arrays/combinationSum.cpp
@@ -12,11 +12,33 @@ void findCombination(int index , int  target, vector<int>&arr,
                      vector<vector<int>>&ans, vector<int> &ds)
 {
   // base case
+  if(target == 0) {
+    ans.push_back(ds);
+    return ;
+  }
+  if (index == arr.size()) return;
 
-                     }
+
+};
 
 int main()
 {
-    
+
+  vector<int>arr =  {2, 3, 6, 7};
+  int target = 7;
+
+
+  vector<vector<int>> ans;
+  vector<int> ds;
+
+
+  findCombination(0, target, arr, ans, ds);
+
+  for (auto& combo : ans)
+  {
+      for (int x : combo) cout << x << " ";
+      cout << endl;
+  }
+
   return 0;
 }
