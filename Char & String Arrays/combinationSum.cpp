0ef9b21e4c80c97f06acb6835d7e7a65e1e8bd85
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-23 19:33:34 +0530

    combination sum using recursion

--- Char & String Arrays/combinationSum.cpp
+++ Char & String Arrays/combinationSum.cpp
@@ -1,15 +1,22 @@
+#include <vector> 
 #include <iostream>
 using namespace std;
 
+// index     — abhi kahan khada hoon
+// target    — abhi kitna aur chahiye
+// arr       — original array
+// ans       — saare valid combinations
+// ds        — current combination jo bana raha hoon
 
+void findCombination(int index , int  target, vector<int>&arr,
+                     vector<vector<int>>&ans, vector<int> &ds)
+{
+  // base case
+
+                     }
 
 int main()
 {
-  int arr[] = {2, 3, 6, 7};
-  int target =  7;
-  int n = 4; 
-
-
-
+    
   return 0;
 }
