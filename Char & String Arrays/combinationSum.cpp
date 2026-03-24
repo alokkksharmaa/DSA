c0c34a90ce6c67e6279a1caf1f31fd6ab7b7ee38
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-24 19:07:35 +0530

    merge sort

--- Char & String Arrays/combinationSum.cpp
+++ Char & String Arrays/combinationSum.cpp
@@ -1,43 +1,51 @@
-#include <vector> 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-// index     — abhi kahan khada hoon
-// target    — abhi kitna aur chahiye
-// arr       — original array
-// ans       — saare valid combinations
-// ds        — current combination jo bana raha hoon
-
-void findCombination(int index , int  target, vector<int>&arr,
-                     vector<vector<int>>&ans, vector<int> &ds)
+void findCombination(vector<int> &arr, int target, int index,
+                     vector<vector<int>> &ans, vector<int> &v)
 {
-  // base case
-  if(target == 0) {
-    ans.push_back(ds);
-    return ;
-  }
-  if (index == arr.size()) return;
 
+  if (index == arr.size())
+  {
+    if (target == 0)
+    {
+      ans.push_back(v);
+    }
+    return;
+  };
 
+  // pick up the element
+  if (arr[index] <= target) // if index of arr <= taret means 4 < 7
+  {
+    v.push_back(arr[index]);
+                         // 7   -  2  = 5 
+    findCombination(arr, target - arr[index], index, ans, v);
+    v.pop_back();
+  };
+
+  findCombination(arr, target, index + 1, ans, v);
 };
 
 int main()
 {
 
-  vector<int>arr =  {2, 3, 6, 7};
+  vector<int> arr = {2, 3, 6, 7};
   int target = 7;
 
-
   vector<vector<int>> ans;
-  vector<int> ds;
+  vector<int> v;
 
+  findCombination(arr, target, 0, ans, v);
 
-  findCombination(0, target, arr, ans, ds);
-
-  for (auto& combo : ans)
+  for (auto &comb : ans)
   {
-      for (int x : combo) cout << x << " ";
-      cout << endl;
+    for (int x : comb)
+    {
+      cout << x << " ";
+    }
+    cout << endl;
   }
 
   return 0;
