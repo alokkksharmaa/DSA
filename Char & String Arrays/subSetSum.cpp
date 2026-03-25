46ad9627263b7665392ec34b96d3a89789b522af
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-25 22:20:00 +0530

    __

--- Char & String Arrays/subSetSum.cpp
+++ Char & String Arrays/subSetSum.cpp
@@ -3,7 +3,22 @@
 #include <iostream>
 using namespace std;
 
-void 
+void generateSubSetSum(int index, int currentSum, const int arr[],
+                        int n, vector<int>&subsetSum)
+{
+
+  // base case :  all elements processesed
+  
+  if(index == n){
+    subsetSum.push_back(currentSum);
+    return;
+  }
+
+  generateSubSetSum(index + 1, currentSum + arr[index] , arr, n , subsetSum);
+
+  generateSubSetSum(index + 1, currentSum , arr, n , subsetSum);
+
+}
 
 int main()
 {
