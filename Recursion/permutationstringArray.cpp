ce366cf1b434eb4926527f1c601d7fd1fcf2fa3d
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-28 11:45:17 +0530

    permutation in string

--- Recursion/permutationstringArray.cpp
+++ Recursion/permutationstringArray.cpp
@@ -6,19 +6,27 @@ void swap(char *x , char *y) {
     temp = *x;
     *x = *y;
     *y = temp;
-};
+}
+void getPermutation(char *a, int left, int right) {
 
-void getPermutation(char *a, int l, int r) {
-
-    
+    int i;
+    if(left == right){
+      cout << a << endl;
+    }else{
+      for(int i= left; i <= right; i++){
+        swap( (a+left), a[right]);
+        getPermutation(a, left+1, right);
+        swap((a+left), (a+i));
+      }
+    }
 }
 int main()
 {
   char str[] = "ABC"; 
 	int n = strlen(str);
-  
 
-        
+  getPermutation(str, 0, n-1);
+
   
   return 0;
 };
