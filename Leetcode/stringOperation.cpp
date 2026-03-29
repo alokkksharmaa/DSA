594e7a1d32ed3551f9e8563eb4e757cc52d780e1
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-29 21:41:42 +0530

    leetcode files

--- Leetcode/stringOperation.cpp
+++ Leetcode/stringOperation.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-bool canBeEqual(string s1, string s2)
+bool canBeEqual(string s1, string s2){
+
+}
 
 int main()
 {
@@ -9,9 +11,3 @@ int main()
   return 0;
 }
 
-
-bool s02=s1[0]==s2[2] && s1[2]==s2[0];
-        bool b02=s1[0]==s2[0] && s1[2]==s2[2];
-        bool s13=s1[1]==s2[3] && s1[3]==s2[1];
-        bool b13=s1[1]==s2[1] && s1[3]==s2[3];
-        return ((s02 ||b02)&&(s13 || b13));
