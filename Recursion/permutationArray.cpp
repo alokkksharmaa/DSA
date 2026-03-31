599c21dc177a0cc77a0b3a9b62329f24bcfa78c5
alokkksharmaa <aloksharma1097@gmail.com>
2026-03-31 22:32:51 +0530

    permutations

---
+++ Recursion/permutationArray.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void helper(vector<int>&arr, int index, vector<vector<int>>&ans){
+
+  // base case
+  if(index >= arr.size()){
+    ans.push_back(arr);
+    return;
+  }
+
+  for(int j = index ; j<arr.size();j++){
+    swap(arr[index], arr[j]);
+    helper(arr, index+1, ans);
+
+    // backtracting
+    swap(arr[index], arr[j]);
+  }
+};
+
+int main()
+{
+  vector<int> arr = {2, 3, 4};
+  int index = 0;
+
+  vector<vector<int>> ans;
+
+  helper(arr, index, ans);
+
+  for (const auto& perm : ans) {
+    for (int val : perm) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+    
+  return 0;
+}
