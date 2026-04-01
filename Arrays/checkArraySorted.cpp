e54d64ba6051a81617a9d68baa72547f8d912c34
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-01 17:02:35 +0530

    Sorted array validation

--- Arrays/checkArraySorted.cpp
+++ Arrays/checkArraySorted.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
 
-bool isSorted(int arr[], int  n ){
+bool isSorted(int arr[], int n)
+{
 
-  int i = 0;
-  for(int i = 0; i<n;i++){
-    
+  for (int i = 0; i < n-1; i++)
+  {
+    if (arr[i] >= arr[i + 1])
+    {
+      return false;
+    }
   }
-
-
+  return true;
 };
 
-int main(){
+int main()
+{
 
   int arr[] = {2, 5, 1, 7, 3};
-  int n     = 5;
+  int n = 5;
 
+  if(!isSorted(arr, n)){
+    cout << " Array is Not sorted!";
+  }else{
+    cout << "Sorted Array";
+  };
+  cout << endl;
 
-
-
-
-  return 0;
+      return 0;
 }
-
-
