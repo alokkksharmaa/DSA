adcc4d9f872a03b2e2da590a40fb678e82e00690
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-03 17:53:28 +0530

    palindrome partining

--- Recursion/palindromePartioning.cpp
+++ Recursion/palindromePartioning.cpp
@@ -1,47 +1,45 @@
-// 131. Palindrome Partitioning
-// Given a string s, partition s such that every substring of the partition is a palindrome.
-//  Return all possible palindrome partitioning of s.
-
 #include <bits/stdc++.h>
 using namespace std;
 
-bool  isPalindrome(string s, int start, int end){
-
-  while(start <= end){
-    if(s[start++] != s[end--])  return false;
-  }
-  return true;
-
-};
-
-void helper(string &s, int index, vector<int>&path, vector<int>&res){
-
-  if(index == s.size()){ 
-    res.push_back(path);
-    return;
-  }
-
-  for(int i=index;i<s.size(); i++){
-    if(isPalindrome(s, index, res)){
-      path.push_back(s.substr(index, i - index+1));
-      helper(s, i+1, path, res);
-      path.pop_back();
+bool isPalindrome(string &s, int start, int end){
+    while(start <= end){
+        if(s[start++] != s[end--]) return false;
     }
-  }
+    return true;
+}
 
+void helper(string &s, int index,
+            vector<string> &path,
+            vector<vector<string>> &res){
 
+    if(index == s.size()){
+        res.push_back(path);
+        return;
+    }
 
+    for(int i = index; i < s.size(); i++){
+        if(isPalindrome(s, index, i)){
+            path.push_back(s.substr(index, i - index + 1));
+            helper(s, i + 1, path, res);
+            path.pop_back();
+        }
+    }
 }
 
 int main(){
 
-  const char s[] = "aab";
+    string s = "aab";
 
-  vector<int> res;
-  vector<int> path;
+    vector<vector<string>> res;
+    vector<string> path;
 
-  helper(s, 0, path , res);
+    helper(s, 0, path, res);
 
+    for(auto x : res){
+      for(string print : x){
+        cout << print <<  " " ;
+      }
+    }
 
-  return 0;
+    return 0;
 }
