f47e44c28264709a0c49b4dc7d15b08e640bb01b
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-09 17:17:30 +0530

    majority element

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -1,15 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 // nums = {3,2,3};
 // majority of it
 
-// map which will store all the elements int key value pair 
-// then I will divide the n by 2, 
+// map which will store all the elements int key value pair
+// then I will divide the n by 2,
+// iterate for the map , first will be number , 2nd will be frequency
+// if second > n
+// return first;
+
+int majorityElement(int nums[], int n) {
+
+  unordered_map<int, int> mp;
+
+  for(int i=0; i<n; i++){
+    mp[nums[i]]++;
+  };
+
+
+  n = n/2;
+
+  for( auto x : mp ){
+
+    if( x.first > n ){
+      return x.second;
+    }
+  }
+};
 
 int main()
 {
 
   int nums[] = {3, 2, 3};
   int n = sizeof(nums) / sizeof(nums[0]);
+
+
+  majorityElement(nums, n);
+
+
+
 }
