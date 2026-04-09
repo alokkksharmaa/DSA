28e1118e271529758bc30b7e0acdebc22bb218ab
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-10 00:06:27 +0530

    rat in a maze problem

--- Recursion/ratInaMaze.cpp
+++ Recursion/ratInaMaze.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<string>> main()
+vector<string> main()
 {
   long long maze[4][4] = {{1, 0, 0, 0}, {1, 1, 0, 1},
                           {1, 1, 0, 0}, {0, 1, 1, 1}};
 
-  vector<vector<string>> ans;
+  vector<string> ans;
 
   // very simple base case;
   if (maze[0][0] == 0)
   {
     return ans;
   };
+ 
+  int visited = 1;
+
 
   int srcx = 0;
   int srcy = 0;
+
+
 
   return ans;
 }
