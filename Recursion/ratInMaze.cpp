2800e49888a2ddeafa66aba30f597e034ae0e594
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-09 15:52:01 +0530

    Rat in A maze

--- Recursion/ratInMaze.cpp
+++ Recursion/ratInMaze.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+class Solution{
+  void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans,
+    string move, vector<vector<int>> &vis){
+
+      if( i == n-1 && j = n-1 ){
+        
+      }
+
+  }
+}
+
+
 
 int main()
 {
@@ -10,7 +24,7 @@ int main()
 
   vector<string> ans;
   vector<vector<int>> vis(n, vector<int> (n, 0));
-  
+
 
   return 0;
 }
