917a37cdf1b45e14b00d5fb0a7907fe53751b130
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-12 22:07:24 +0530

    leetcode contest

--- Leetcode/contest.cpp
+++ Leetcode/contest.cpp
@@ -2,3 +2,29 @@
 using namespace std;
 
 // leetcode contest 12 Apr, Sunday 2026 
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+
+      int n = matrix.size();
+      vector<int> res (n, 0);
+
+     for(int i =0;i<n;i++){
+       int degree = 0;
+
+       for(int j = 0; j <n; j++){
+         if(matrix[i][j] == 1){
+           degree++;
+         }
+       }
+       res[i] = degree;
+     }
+
+      return res;
+      
+    }
+};
