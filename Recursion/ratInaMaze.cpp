c6312159bbe765c06512e4c055577d3ec113645a
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-13 20:43:07 +0530

    rat in a maze

--- Recursion/ratInaMaze.cpp
+++ Recursion/ratInaMaze.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve(int i, int j, int maze[4][4], string path, vector<string> &ans, int n)
+{
 
-void solve( int maze[4][4], vector<vector<int>> ans, int n){
-
-
+  if (i == n - 1 && j == n - 1)
+  {
+    ans.push_back(path);
+    return;
+  }
 }
 
 vector<string> main()
 {
-  int maze[4][4] = {{1, 0, 0, 0}, {1, 1, 0, 1},
-                          {1, 1, 0, 0}, {0, 1, 1, 1}};
+  int maze[4][4] = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
 
-  size_t n = sizeof(maze) / sizeof(maze[0][0]); 
+  size_t n = sizeof(maze) / sizeof(maze[0][0]);
   vector<string> ans;
 
   // very simple base case;
@@ -20,20 +23,13 @@ vector<string> main()
   {
     return ans;
   };
- 
-  int visited = 1;
 
+  int visited = 1;
 
   int srcx = 0;
   int srcy = 0;
 
-
-  string path =  " ";
-
-
-
-
-
+  string path = " ";
 
   return ans;
 }
