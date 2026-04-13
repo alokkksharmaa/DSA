fe9d2b63ad19838682fe7ac83f5f41241cfe48df
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-13 22:36:58 +0530

    rat in a maze

--- Recursion/ratInaMaze.cpp
+++ Recursion/ratInaMaze.cpp
@@ -26,70 +26,28 @@ using namespace std;
     visited      -> tracks visited cells to avoid cycles
 */
 
-void findPaths(int row,int col,vector<vector<int>>& grid,int n,vector<string>& paths,
-               string currentPath,vector<vector<int>>& visited)
+void findPaths
+
+    int
+    main()
 {
-    // Destination reached
-    if (row == n - 1 && col == n - 1)
-    {
-        paths.push_back(currentPath);
-        return;
-    }
 
-    // Mark current cell as visited
-    visited[row][col] = 1;
+  vector<vector<int>> maze{
+      {1, 0, 0, 0},
+      {1, 1, 0, 0},
+      {1, 1, 0, 0},
+      {0, 1, 1, 1},
+  };
 
-    // Move Down
-    if (row + 1 < n && !visited[row + 1][col] && grid[row + 1][col] == 1){
-      findPaths(row + 1,col,grid,n,paths,currentPath + 'D',visited);
-    }
+  // determine it's size
+  int n = sizeof(maze) / sizeof(maze[0][0]);
 
-    // Move Left
-    if (col - 1 >= 0 && !visited[row][col - 1] && grid[row][col - 1] == 1){
-        findPaths(row,col - 1,grid,n, paths,currentPath + 'L',visited);
-    }
+  // visited path, another grid of visited
+  vector<vector<int>> visited(n, vector<int>(n, 0));
 
-    // Move Right
-    if (col + 1 < n && !visited[row][col + 1] && grid[row][col + 1] == 1){
-        findPaths(row, col + 1,grid,n,paths,currentPath + 'R',visited);
-    }
+  string path;
 
-    // Move Up
-    if (row - 1 >= 0 && !visited[row - 1][col] && grid[row - 1][col] == 1){
-        findPaths(row - 1,col,grid,n,paths,currentPath + 'U',visited);     
-    }
-
-    // Backtrack
-    visited[row][col] = 0;
-}
-
-int main()
-{
-    vector<vector<int>> grid =
-    {
-        {1, 0, 0, 0},
-        {1, 1, 0, 1},
-        {1, 1, 0, 0},
-        {0, 1, 1, 1}
-    };
-
-    int n = grid.size();
-
-    vector<string> paths;
-
-    // Visited matrix to prevent revisiting cells
-    vector<vector<int>> visited(n,vector<int>(n, 0));
-
-    // Start only if source is open
-    if (grid[0][0] == 1){
-        findPaths(0,0,grid,n,paths,"",visited);
-    }
-
-    // Print paths
-    for (auto& path : paths)
-    {
-        cout << path << " " << endl;
-    }
-
-    return 0;
+  if (maze[0][0] == 1)
+  {
+  }
 }
