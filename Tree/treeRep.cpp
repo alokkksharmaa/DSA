0c7bea50310222ee09dd6fe8084b5c9e5258fd96
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-14 13:06:24 +0530

    rat in a maze

---
+++ Tree/treeRep.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+
+  int data;
+  Node*left;
+  Node*right;
+
+  Node(int value){
+    data = value;
+    left = right = nullptr;
+  }
+
+};
+
+
+int main()
+{
+
+  struct Node* root = new Node(1);
+
+  root -> left = new Node(2);
+  root -> right =  new Node(3);
+  root -> left -> right = new Node(4);
+
+  
+    
+  return 0;
+}
