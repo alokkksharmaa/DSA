73805f2dc7984772f1d94bf509709df07b8f0a88
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-14 21:13:57 +0530

    rat in a maze

--- Tree/preOrder.cpp
+++ Tree/preOrder.cpp
@@ -13,19 +13,13 @@ struct Node{
 };
 
 
-void preOrder(Node){
+void preOrder(Node* root)
+{
+  if(root == nullptr) return; 
 
-  if(Node == nullptr){
-    return;
-  } 
-
-
-  // preorder = Root, Left, Right
-  // print(node -> data);
-
-
-
-
+  cout << root -> data << " ";
+  preOrder(root -> left);
+  preOrder(root -> right);
 }
 
 
@@ -33,6 +27,15 @@ int main()
 {
 
   struct Node* root = new Node(1);
+  root -> left = new Node(2);
+  root -> right = new Node(3);
+  root -> left -> left = new Node(4);
+  root -> left -> right = new Node(5);
+  root -> right -> left = new Node(6);
+  root -> right -> right = new Node(7);
+  cout << "Preorder Traversal: ";
+  preOrder(root);
+
 
 
     
