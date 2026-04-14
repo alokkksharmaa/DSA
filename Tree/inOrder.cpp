da325919006f364799fd8d435ff5f4eb5a733db6
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-14 22:28:35 +0530

    inOrder traversal

--- Tree/inOrder.cpp
+++ Tree/inOrder.cpp
@@ -15,11 +15,33 @@ struct Node
   }
 };
 
+void inOrder(Node *root)
+{
 
+  if (root == nullptr)
+    return;
 
+  // left -> root -> right
+  inOrder(root->left);
+  cout << root->data << " ";
+  inOrder(root->right);
+}
 
 int main()
 {
+  struct Node *root = new Node(1);
+
+  root->left = new Node(2);
+  root->left->left = new Node(4);
+  root->left->right = new Node(7);
+  root->left->left->left = new Node(11);
+  root->left->left->right = new Node(8);
+
+  root->right = new Node(3);
+  root->right->right = new Node(5);
+  root->right->left = new Node(6);
+
+  inOrder(root);
 
   return 0;
 }
