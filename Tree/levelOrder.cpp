634ab894d403972640b34415319da8836d3f8d9a
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-14 22:52:42 +0530

    level order traversal

---
+++ Tree/levelOrder.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+//  Simulate the level order traversal of a tree (BFS);
+/*
+            1
+          /   \
+         2     3
+        / \   / \
+       4   7 6   5
+      / \
+     11  8
+
+*/
+
+// output:- 1, 2, 3, 4, 7, 6, 5, 11, 8
+
+struct Node
+{
+
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = nullptr;
+  }
+};
+
+void levelOrder(Node *root)
+{
+
+  if (root == nullptr)
+    return;
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    Node *curr = q.front();
+    q.pop();
+
+    cout << curr->data << " ";
+
+    if (curr->left != nullptr)
+    {
+      q.push(curr->left);
+    }
+
+    if (curr->right != nullptr)
+    {
+      q.push(curr->right);
+    }
+  }
+};
+
+int main()
+{
+  struct Node *root = new Node(1);
+
+  root->left = new Node(2);
+  root->left->left = new Node(4);
+  root->left->right = new Node(7);
+  root->left->left->left = new Node(11);
+  root->left->left->right = new Node(8);
+
+  root->right = new Node(3);
+  root->right->right = new Node(5);
+  root->right->left = new Node(6);
+
+  levelOrder(root);
+
+  return 0;
+}
---
+++ Tree/postOrder.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node{
+  int data;
+  Node*left;
+  Node*right;
+
+  Node(int val){
+    data = val;
+    left = right = nullptr;
+  }
+};
+
+void postOrder(Node* root){
+
+  if(root == nullptr) return;
+
+
+  postOrder(root ->  left);
+  postOrder(root -> right);
+  cout << root -> data << " ";
+
+}
+
+int main()
+{
+
+  struct Node* root  = new Node(1);
+
+  root -> left = new Node(2);
+  root->left->left = new Node(4);
+  root->left->right = new Node(7);
+
+  root->right = new Node(3);
+  root->right->right = new Node(5);
+  root->right->left = new Node(6);
+
+  postOrder(root);
+    
+  return 0;
+}
