6054a1330eb70227e646adc44820a1d652ca8c8f
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-16 16:30:27 +0530

    balanced binary tree

---
+++ Tree/balancedBinaryTree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node {
+  int data;
+  Node* left;
+  Node* right;
+
+  Node(int val){
+    data = val;
+    left = right = nullptr;
+  }
+};
+
+bool isBalanced(Node* root){
+
+  return dfsHeight(root) != -1;
+}
+  int dfsHeight(Node* root){
+
+    
+
+  }
+
+
+int main()
+{
+
+  struct Node* root = new Node(2);
+  root -> left = new Node(19);
+  root->left->left = new Node(4);
+  root->left->right = new Node(7);
+
+  root->right = new Node(3);
+  root->right->right = new Node(5);
+  root->right->left = new Node(6);
+
+    
+  return 0;
+}
