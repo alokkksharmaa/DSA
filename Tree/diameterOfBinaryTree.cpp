a95190ee64378a49ae55e7b9b2a055e47ce5c950
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-17 21:44:04 +0530

    diameter of a binary tree

---
+++ Tree/diameterOfBinaryTree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int data;
+  Node* left;
+  Node* right;
+
+  Node(int val){
+    data = val;
+    left = right = nullptr;
+  }
+};
+
+int diameter(Node* root){
+
+}
+
+
+int main()
+{
+  struct Node* root = new Node(2);
+  
+
+  
+  return 0;
+}
