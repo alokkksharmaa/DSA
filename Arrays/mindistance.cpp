92868dcc84d853249210fca8103a96dd2ec8f9e3
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-19 15:08:20 +0530

    minimum distance

--- Arrays/mindistance.cpp
+++ Arrays/mindistance.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int maxDistance(vector<int> &nums1, vector<int> &nums2)
+{
+  int i = 0, j = 0;
+  int ans = 0;
 
-class Solution {
-  public:
-      int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-          int i = 0, j = 0;
-          int ans = 0;
-  
-          while(i < nums1.size() && j < nums2.size()){
-              if(nums1[i] <= nums2[j]){
-                  ans = max(ans, j - i);
-                  j++;
-              }else{
-                  i++;
-                  if(i > j) j = i;
-              }
-          }
-  
-          return ans;
-      }
-  };
+  while (i < nums1.size() && j < nums2.size())
+  {
+    if (nums1[i] <= nums2[j])
+    {
+      ans = max(ans, j - i);
+      j++;
+    }
+    else
+    {
+      i++;
+      if (i > j)
+        j = i;
+    }
+  }
 
-int main(){
+  return ans;
+}
+}
+;
 
-  
+int main()
+{
 
+  vector<int> nums1 = {55, 30, 5, 4, 2};
+  vector<int> nums2 = {100, 20, 10, 10, 5};
 
   return 0;
 }
