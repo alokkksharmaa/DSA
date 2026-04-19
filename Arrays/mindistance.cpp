8b449c958c009934dd7e2899d877c5ebc9fa5694
alokkksharmaa <aloksharma1097@gmail.com>
2026-04-19 15:06:41 +0530

    minimum distance

--- Arrays/mindistance.cpp
+++ Arrays/mindistance.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+
+class Solution {
+  public:
+      int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+          int i = 0, j = 0;
+          int ans = 0;
+  
+          while(i < nums1.size() && j < nums2.size()){
+              if(nums1[i] <= nums2[j]){
+                  ans = max(ans, j - i);
+                  j++;
+              }else{
+                  i++;
+                  if(i > j) j = i;
+              }
+          }
+  
+          return ans;
+      }
+  };
+
 int main(){
 
   
